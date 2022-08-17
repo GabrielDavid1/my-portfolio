717dca6f265400dbf8622bfddeb6f3079382e9f8
gabrieldavid <gabrieldavid75315@gmail.com>
2022-08-17 12:40:09 -0300

    translate utils has finished

---
+++ src/utils/Translate.ts
@@ -0,0 +1,85 @@
+export default [
+    {
+        presentation: {
+            navTitle_EN: 'Initial',
+            navTitle_PT: 'Inicial',
+            title: 'Welcome to my portfolio',
+            subtitle: `My name is gabriel, I'm 23 years old, thanks to the creator of heaven and earth I have a great passion to put all my ideas into practice. Passionate about challenges, solving problems and developing projects, I consider myself very curious because I really enjoy learning new subjects even if they have nothing to do with my area of ​​expertise.`,
+        },
+        projects: {
+            navTitle_EN: 'Projects',
+            navTitle_PT: 'Projetos',
+            Contents: [
+                {
+                    BR_description: 'Projetor de tela, com este projeto é possível manipular elementos e através de eventos alterar propriedades como cor, largura, altura, margem e etc.',
+                    EN_description: 'Screen projector, with this project it is possible to manipulate elements and through events change properties such as color, width, height, margin and etc.',
+                },
+                {
+                    BR_description: 'É um projeto de desafio onde estudei Vuejs por 1 dia baseado em documentação / tutorial e no dia seguinte construí uma aplicação com vue e Vuetify.',
+                    EN_description: `It's a challenge project where I studied vuejs for 1 day based on documentation / tutorial and the next day I built an application with vue and vuetify.`,
+                },
+                {
+                    BR_description: 'Projeto para usar criptomoeda como forma de pagamento em favor do blockchain. O objetivo era apenas construir o back-end, mas acabei integrando com o front-end também.',
+                    EN_description: 'Project to use cryptocurrency as a payment method in favor of blockchain. The goal was just to build the back-end but I ended up integrating with the front-end as well.',
+                },
+                {
+                    BR_description: 'Api construída para meu projeto de pagamentos de criptomoedas não blockchain, possui cadastro de usuários, sistema de token e atualização de token, autenticação, sistema de persistência de pagamento e consulta à api binance.',
+                    EN_description: 'Api built for my non-blockchain cryptocurrency payments project, it has user registration, token and refresh token system, authentication, payment persistence system and query to the binance api.',
+                },
+                {
+                    BR_description: 'Projeto mobile para gestão de produtos e controle de estoque. Meu foco nesse projeto, além de colocar em prática o conhecimento mobile, foi também aprimorar minha noção de ux design.',
+                    EN_description: 'Mobile project for product management and stock control. My focus on this project, in addition to putting mobile knowledge into practice, was also to improve my notion of ux design.',
+                },
+                {
+                    BR_description: 'Meu melhor projeto em termos de código, planejamento e desempenho. Usei meu conhecimento de meses para focar neste projeto que visa projetar telas mobile e converter código web para nativo (react native).',
+                    EN_description: 'My best project in terms of code, planning and performance. I used my knowledge of months to focus on this project that aims to design mobile screens and convert web code to native (react native).',
+                },
+                {
+                    BR_description: 'Projeto modelo para colocar em prática noções de ux design. Construí um modelo para um aplicativo de gestão financeira, vendendo / comprando produtos.',
+                    EN_description: 'Template project to put into practice ux design notions. I built a model for a financial management app, selling/buying products.',
+                },
+                {
+                    BR_description: 'Projeto de um aplicativo de pizzaria feito no meu curso de desenvolvimento full stack.',
+                    EN_description: 'Project of a pizzeria app made in my full stack development course.',
+                },
+                {
+                    BR_description: 'Projeto para um aplicativo de aluguel de carros feito no meu curso de desenvolvimento full stack.',
+                    EN_description: 'Project for a car rental application made in my full stack development course.',
+                },
+                {
+                    BR_description: 'Projeto de carrinho de compras feito no meu curso de desenvolvimento full stack.',
+                    EN_description: 'Shopping cart project done in my full stack development course.',
+                },
+                {
+                    BR_description: 'Apenas meu primeiro deploy feito na vercel.',
+                    EN_description: 'Just my first deploy made on vercel.',
+                },
+            ]
+        },
+        improvement: {
+            navTitle_EN: 'Improvement',
+            navTitle_BR: 'Melhorias',
+            title: 'Apesar de ter melhorado muito em 2022, vejo que tenho pontos a melhorar para chegar a um nível excelente como profissional, esses pontos são:',
+            content: [
+                {
+                    title_EN: 'UX Design',
+                    title_BR: 'Design UX',
+                    subTitle_EN: 'My notions in this modality were dark in 2022, it evolves a lot in this regard, but I have a lot to go through if you consider the high-level projects that are developed by excellent professionals.',
+                    subTitle_BR: 'Minhas noções nesta modalidade eram tenebrosas em 2022 evolui muito neste quesito porém tenho muito oque percorrer se considerar os projetos de alto nivel que são desenvolvidos por profissionais excelentes.',
+                },
+                {
+                    title_EN: 'Search',
+                    title_BR: 'Pesquisa',
+                    subTitle_EN: 'The scope of research involves understanding methodologies to map the best paths to problem solving.',
+                    subTitle_BR: 'O ambito da pesquisa envolve metodologias de compreensão para mapear os melhores caminhos para a solução de problemas.',
+                },
+                {
+                    title_EN: 'Process',
+                    title_BR: 'Processo',
+                    subtitle_EN: 'I will study more depth about the processes before the code to increase the accuracy in the creation of tools.',
+                    subtitle_BR: 'Pretendo estudar mais a fundo sobre os processos antes do código para aumentar a precisão na criação das ferramentas.',
+                },
+            ],
+        }
+    }
+];
